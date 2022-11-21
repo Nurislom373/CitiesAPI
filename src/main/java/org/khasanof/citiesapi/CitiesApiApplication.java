18f62bf4f0151d85f2ad17790239360483a863af
khasanof373 <khasanof373@gmail.com>
2022-11-21 11:44:00 +0500

    start

---
+++ src/main/java/org/khasanof/citiesapi/CitiesApiApplication.java
@@ -0,0 +1,13 @@
+package org.khasanof.citiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitiesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/khasanof/citiesapi/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khasanof.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
